fd9afa4b0f6c0acd11116016257cd64f0ce11397
Joe Van Dyk <joe@fixieconsulting.com>
2012-09-25 17:20:56 -0700

    wip

--- rooster_pg_enum.rb
+++ rooster_pg_enum.rb
@@ -74,3 +74,25 @@ module Rooster
     end
   end
 end
+
+
+# From https://github.com/RISCfuture/enum_type/blob/master/lib/enum_type/extensions.rb
+# Patches AR to correctly set the default.
+class ActiveRecord::ConnectionAdapters::PostgreSQLColumn
+  def initialize(name, default, sql_type = nil, null = true)
+    super(name, self.class.extract_value_from_default(default, sql_type), sql_type, null)
+  end
+
+  def self.extract_value_from_default_with_enum(default, type)
+    case default
+      when /\A'(.*)'::(?:#{Regexp.escape type})\z/
+        $1
+      else
+        extract_value_from_default_without_enum default
+    end
+  end
+
+  class << self
+    alias_method_chain :extract_value_from_default, :enum
+  end
+end if ActiveRecord::ConnectionAdapters::PostgreSQLColumn.methods.include?(:extract_value_from_default)
